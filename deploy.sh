1a5a1ee949adc047c3a77289662f70ef9ff95b91
Riley Shahar <riley.shahar@gmail.com>
2023-08-04 18:07:13 -0400

    Test github pages deployment

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find output/* -not -name ".git" -not -path "*/.git/*" | xargs rm -rf
-./build.sh
-
-cd output
-git add .
-git commit -m "deploy @ $(git rev-parse --short HEAD)"
-git push
